973d021252c9d81b97d67b11819889bfbc7c8da8
PauloVLB <vitorlimaborges@gmail.com>
2020-12-15 16:39:21 -0300

    Chaves (OBI)

--- OBI/2016/chaves.cpp
+++ OBI/2016/chaves.cpp
@@ -1,24 +1,16 @@
 #include <bits/stdc++.h>
-
-#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
 using namespace std;
 
-string l;
-int x;
-
-int main() {_
-    while(getline(cin, l)){
-        for(int i = 0; i < l.size(); i++) {
-            if(l[i] == '{') {
-                x++;
-            } else if(l[i] == '}') {
-                x--;
-            }
-
-            if(x < 0) x = 0;
-        }
-    }
-    cout << (x == 0 ? 'S' : 'N') << endl;
-    return 0;
+int main()
+{
+	int a = 0;
+	string s;
+	while (getline(cin, s))
+	{
+		for (int i = 0; i < s.size() && a >= 0; ++i)
+			a += s[i] == '{' ? 1 : (s[i] == '}' ? -1 : 0);
+		if (a < 0) break;
+	}
+	cout << (a == 0 ? 'S' : 'N') << endl;
 }
+
