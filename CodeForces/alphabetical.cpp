501d490ab3df58e8defd461784ef194c79b81ea1
PauloVLB <vitorlimaborges@gmail.com>
2021-07-12 18:26:17 -0300

    Alphabetical Strings, Obstacel - CodeForces

---
+++ CodeForces/alphabetical.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+string al = "abcdefghijklmnopqrstuvwxyz";
+int t; 
+string s;
+
+int main() {_
+    cin >> t;
+
+    while(t--) {
+        cin >> s;
+
+        int t = s.size();
+        string ns = "a";
+        int tn = 0;
+        for(char c : al) {
+            if(tn == t-1) break;
+            int p = s.find(c);
+            char next = c+1;
+
+            if(s.find(next) > p) {
+                ns = ns + next;
+            } else if(s.find(next) < p) {
+                ns = next + ns;
+            }
+            tn++;
+        }
+        cout << (ns == s ? "YES" : "NO") << endl;
+    }
+}
---
+++ CodeForces/obstacle.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int t, n, xa, xb, ya, yb, xf, yf;
+
+int main() {_
+    cin >> t;
+
+    while(t--) {
+        cin >> xa >> ya >> xb >> yb >> xf >> yf;
+
+        int ans = abs(xa - xb) + abs(yb - ya);
+        
+        if((xa == xb && xb == xf) && yf > min(ya, yb) && yf < max(ya, yb)) {
+            ans += 2;
+        }
+        if((ya == yb && yb == yf) && xf > min(xa, xb) && xf < max(xa, xb)) {
+            ans += 2;
+        }
+
+        cout << ans << endl;
+    }
+}
