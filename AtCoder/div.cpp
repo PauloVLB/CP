aaef926655eea26fa2bd7bc14bb829ccf1c21f50
PauloVLB <vitorlimaborges@gmail.com>
2021-08-11 21:59:35 -0300

    Div - AtCoder

--- AtCoder/div.cpp
+++ AtCoder/div.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 
 #define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl '\n'
 
 using namespace std;
 
-int a, b;
+int n;
 
 int main() {_ 
-    cin >> a >> b;
+    cin >> n;
 
-    cout << (a^b) << endl;
+    cout << n-1 << endl;
 }
