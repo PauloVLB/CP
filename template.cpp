8ca188613efd10780c4a0a2a507392529180b390
PauloVLB <vitorlimaborges@gmail.com>
2021-08-21 08:57:51 -0300

    Atualiza template

--- template.cpp
+++ template.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 
 #define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
 
 using namespace std;
 
-int main() {_
+int main() {_ 
 
-    return 0;
 }
