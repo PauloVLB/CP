1b6350b7f79fe5c5f1f5e9fe869985c014a98493
PauloVLB <vitorlimaborges@gmail.com>
2022-04-11 22:24:21 -0300

    Add Filter EBD1

---
+++ EDB1/filter/filter.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int* filter(int * first, int * last) {
+    int * current = first;
+
+    while(first < last && current < last) {
+        if(*current > 0) {
+            std::swap(*current, *first);
+            first++;
+        }
+        current++;
+    }
+
+    return first;
+}
+
+int main() {
+    int a[10] = {-2, -8, 6, 7, -3, 10, 1, 0, -3, 7};
+
+    auto p = filter(a, a+10);
+
+    auto it = std::begin(a);
+
+    while(it < p) {
+        std::cout << *it << ' ';
+        it++;
+    }
+
+    std::cout << '\n';
+}
